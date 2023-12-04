1a5364a7f2fe421f69e83119333226ca8ca01d45
Andrey Rudenko <me@prepor.dev>
2023-12-04 20:05:44 +0100

    p2 naive solution (doesn't work for real input)

--- prolog/4.pl
+++ prolog/4.pl
@@ -1,4 +1,4 @@
-:- autoload(library(lists), [member/2, sum_list/2]).
+:- autoload(library(lists), [member/2, sum_list/2, nth1/3]).
 :- autoload(library(dcg/basics), [integer/3, blanks/2]).
 :- autoload(library(readutil), [read_file_to_string/3]).
 
@@ -11,14 +11,20 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11').
 
 file_input(I) :-
-    read_file_to_string("/Users/prepor/Dropbox/lab/aoc2023/files/input4.txt", I, []).
+    read_file_to_string("/Users/prepor/Dropbox/lab/aoc2023/files/tt.txt", I, []).
 
 input(I) :- file_input(I).
+% input(I) :- example1(I).
+
+
+% -- SOLUTIONS
+% Part 1: points_sum(Sum).
 
 % -- API
 
 card(Id, Winning, Numbers) :-
     card_line(L), atom_codes(L, Codes), phrase(p_card(Id, Winning, Numbers), Codes).
+
 
 points(card(Id, Winning, Numbers), Points) :-
     card(Id, Winning, Numbers),
@@ -28,6 +34,68 @@ points(card(Id, Winning, Numbers), Points) :-
 points_sum(Sum) :-
     findall(Points, points(_, Points), L),
     sum_list(L, Sum).
+
+won_by(Id, WonId) :-
+   card(Id, Winning, Numbers),
+   matching_numbers(Winning, Numbers, MatchingNumbers),
+   won_id(Id, MatchingNumbers, WonId).
+
+all_cards_count(N) :-
+    bagof(_, all_cards, B),
+    length(B, N).
+
+all_cards() :- all_cards(_).
+all_cards(Id) :- card(Id, _, _).
+all_cards(Id) :- won_by(Id, WonId), all_cards(WonId).
+
+% all_cards_count(N) :-
+%     findall(_, all_cards, L),
+%     length(L, N).
+
+% all_cards(Count) :- all_cards(1, Count).
+% all_cards(Id, Count) :-
+%     card(Id, _, _),
+%     won_ids(Id, WonIds),
+    
+%     NextId is Id + 1,
+%     all_cards(NextId, NextCount),
+%     Count is 1 + NextCount.
+% all_cards(Id) :- won_by(Id, WonId), all_cards(WonId).
+
+
+
+
+
+% copies_from(Id, WonId) :-
+%     card(Id, Winning, Numbers),
+%     matching_numbers(Winning, Numbers, MatchingNumbers),
+%     won_id(Id, MatchingNumbers, WonId).
+
+% copies_from(Id, WonId) :-
+%     copies_from(Id, CopyId),
+%     card(CopyId, Winning, Numbers),
+%     matching_numbers(Winning, Numbers, MatchingNumbers),
+%     won_id(CopyId, MatchingNumbers, WonId).
+    
+
+% copy(Id) :-
+%     PrevId is Id - 1,
+%     card(PrevId, )
+
+% won_by(Id, WonId) :-
+%     won_by(WonId, )
+
+% cards_copy(WonId) :-
+    % card(Id, Winning, Numbers),
+    % matching_numbers(Winning, Numbers, MatchingNumbers),
+    % won_id(Id, MatchingNumbers, WonId).
+
+% cards_copy(WonId) :-
+%     cards_copy(Id),
+%     card(Id, Winning, Numbers),
+%     matching_numbers(Winning, Numbers, MatchingNumbers),
+%     won_id(Id, MatchingNumbers, WonId).
+
 
 % -- PARSER
 
@@ -62,3 +130,19 @@ calc_points([], 0).
 calc_points([_], 1) :- !.
 calc_points([_|Tail], Points) :-
     calc_points(Tail, PrevPoints), Points is PrevPoints * 2.
+
+% -- WON CARDS
+
+won_id(PrevId, MatchingCards, WonId) :-
+    nth1(I, MatchingCards, _),
+    WonId is PrevId + I.
+
+won_ids(Id, WonIds) :-
+    card(Id, Winning, Numbers),
+    matching_numbers(Winning, Numbers, MatchingNumbers),
+    won_ids(Id, MatchingNumbers, WonIds).
+won_ids(Id, [], []).
+won_ids(Id, [_|MatchingNumbers], [WonId|Acc]) :-
+    length(MatchingNumbers, I),
+    WonId is I + 2,
+    won_ids(Id, MatchingNumbers, Acc).
